121f7510c62ef6b809c6743c063e0ab83de1f369
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2022-08-06 19:33:28 +0900

    いい感じ

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem.Controls;
 
 public class Controller : MonoBehaviour {
 
+    public GameObject bpmSyncer;
     public GameObject cube;
 
     // Start is called before the first frame update
@@ -23,8 +24,42 @@ public class Controller : MonoBehaviour {
             o.SetActive(true);
         }
 
+        if (Keyboard.current.aKey.wasPressedThisFrame) {
+            GameObject o = Instantiate(cube, new Vector3(0.0f, -1.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer o3d = o.GetComponent<Object3DTransformer>();
+            o3d.action = () => o3d.TranslateUp();
+            o.SetActive(true);
+        }
+
+        if (Keyboard.current.sKey.wasPressedThisFrame) {
+            GameObject o = Instantiate(cube, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer o3d = o.GetComponent<Object3DTransformer>();
+            o3d.action = () => o3d.TranslateDown();
+            o.SetActive(true);
+        }
+
+        if (Keyboard.current.dKey.wasPressedThisFrame) {
+            GameObject o = Instantiate(cube, new Vector3(-1.666666f, 0.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer o3d = o.GetComponent<Object3DTransformer>();
+            o3d.action = () => o3d.TranslateRight();
+            o.SetActive(true);
+        }
+
+        if (Keyboard.current.fKey.wasPressedThisFrame) {
+            GameObject o = Instantiate(cube, new Vector3(1.666666f, 0.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer o3d = o.GetComponent<Object3DTransformer>();
+            o3d.action = () => o3d.TranslateLeft();
+            o.SetActive(true);
+        }
+
         if (Keyboard.current.vKey.wasPressedThisFrame) {
-            BPMSyncer.ResetTime();
+            if(bpmSyncer.activeSelf) {
+                BPMSyncer.ResetTime();
+            } else {
+                bpmSyncer.SetActive(true);
+                BPMSyncer.ResetTime();
+            }
+        
         }
         
     }
