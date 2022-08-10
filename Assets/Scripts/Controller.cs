6b9b00533d65510a50503668f3fc2970c71cd5f3
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2022-08-10 16:13:42 +0900

    midi

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,47 +7,59 @@ using UnityEngine.InputSystem.Controls;
 
 public class Controller : MonoBehaviour {
 
+    PlayerInput _input;
+
     public GameObject bpmSyncer;
     public GameObject cube;
     public GameObject camera;
     
     private Material mat;
 
+    private void Awake() {
+
+        TryGetComponent(out _input);
+        
+    }
+
     private void Start() {
 
         mat = camera.GetComponent<CameraFilter>().mat;
         mat.SetColor("_Col", new Color(0.0f, 0.0f, 1.0f, 1.0f));
     }
+  
+    private void OnEnable() {
+        _input.actions["BPMSync"].performed += onBPMSync;
 
-    // Update is called once per frame
-    void Update() {
-            
-        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+        _input.actions["ShakePosition"].performed += onShakePosition;
+        _input.actions["TranslateUp"].performed += onTranslateUp;
+        _input.actions["TranslateDown"].performed += onTranslateDown;
+        _input.actions["TranslateRight"].performed += onTranslateRight;
+        _input.actions["TranslateLeft"].performed += onTranslateLeft;
+
+    }
+
+    private void OnDisable() {
+        _input.actions["BPMSync"].performed -= onBPMSync;
+        
+        _input.actions["ShakePosition"].performed -= onShakePosition;
+        _input.actions["TranslateUp"].performed -= onTranslateUp;
+        _input.actions["TranslateDown"].performed -= onTranslateDown;
+        _input.actions["TranslateRight"].performed -= onTranslateRight;
+        _input.actions["TranslateLeft"].performed -= onTranslateLeft;
+
+    }
+
+    //-----------------------------------------------------------------
+    //-----------------------------------------------------------------
+    private void onShakePosition(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
             GameObject o = Instantiate(cube, Vector3.zero, Quaternion.identity);
             Object3DTransformer.ShakePosition(o);
-        }
+        };
+    }
 
-        if (Keyboard.current.aKey.wasPressedThisFrame) {
-            GameObject o = Instantiate(cube, new Vector3(0.0f, -1.0f, 0.0f), Quaternion.identity);
-            Object3DTransformer.TranslateUp(o);
-        }
-
-        if (Keyboard.current.sKey.wasPressedThisFrame) {
-            GameObject o = Instantiate(cube, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
-            Object3DTransformer.TranslateDown(o);
-        }
-
-        if (Keyboard.current.dKey.wasPressedThisFrame) {
-            GameObject o = Instantiate(cube, new Vector3(-1.666666f, 0.0f, 0.0f), Quaternion.identity);
-            Object3DTransformer.TranslateRight(o);
-        }
-
-        if (Keyboard.current.fKey.wasPressedThisFrame) {
-            GameObject o = Instantiate(cube, new Vector3(1.666666f, 0.0f, 0.0f), Quaternion.identity);
-            Object3DTransformer.TranslateLeft(o);
-        }
-
-        if (Keyboard.current.vKey.wasPressedThisFrame) {
+    private void onBPMSync(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
             if(bpmSyncer.activeSelf) {
                 BPMSyncer.ResetTime();
             } else {
@@ -55,7 +67,34 @@ public class Controller : MonoBehaviour {
                 BPMSyncer.ResetTime();
             }
         }
-        
+    }
+
+    private void onTranslateUp(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
+            GameObject o = Instantiate(cube, new Vector3(0.0f, -1.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer.TranslateUp(o);
+        }
+    }
+
+    private void onTranslateDown(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
+            GameObject o = Instantiate(cube, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer.TranslateDown(o);
+        }
+    }
+
+    private void onTranslateRight(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
+            GameObject o = Instantiate(cube, new Vector3(-1.666666f, 0.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer.TranslateRight(o);
+        }
+    }
+    
+    private void onTranslateLeft(InputAction.CallbackContext obj) {
+        if(obj.ReadValue<float>() == 1) {
+            GameObject o = Instantiate(cube, new Vector3(1.666666f, 0.0f, 0.0f), Quaternion.identity);
+            Object3DTransformer.TranslateLeft(o);
+        }
     }
 
 }
