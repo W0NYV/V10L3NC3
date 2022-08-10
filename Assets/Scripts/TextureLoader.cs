c5c3e21287a7b9441366be755374c5d4d3c940fe
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2022-08-10 19:46:34 +0900

    DD YEAH

---
+++ Assets/Scripts/TextureLoader.cs
@@ -0,0 +1,63 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using B83.Win32;
+
+public class TextureLoader : MonoBehaviour {
+
+    public Material mat;
+    public Texture2D defaultTex;
+    public RawImage rawimage;
+
+    string str;
+
+    private void OnEnable() {
+        UnityDragAndDropHook.InstallHook();
+        UnityDragAndDropHook.OnDroppedFiles += OnDroppedFiles;
+    }
+
+    private void OnDisable() {
+        UnityDragAndDropHook.UninstallHook();
+    }
+
+    void OnDroppedFiles(List<string> aFiles, POINT aPos) {
+        str = aFiles[0];
+        str.Replace(@"\", @"\\");
+        var imageBytes = File.ReadAllBytes(str);
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage(imageBytes);
+        
+        rawimage.texture = tex;
+        mat.SetTexture("_MainTex", tex);
+    }
+
+    private void OnGUI() {
+        GUI.skin.label.fontSize = 50;
+
+        GUILayout.Label($"file: {str}");
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        mat.SetTexture("_MainTex", defaultTex);
+        rawimage.texture = defaultTex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateTexture(string path) {
+
+        var imageBytes = File.ReadAllBytes(path);
+        Debug.Log(imageBytes);
+
+    }
+
+}
